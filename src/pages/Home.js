a6ffadcbce4ad0c283d681ac433480a5b61b347d
bhagwat7540 <bhagwatmiglani@gmail.com>
2021-08-08 11:43:38 +0530

    fetched rsukt

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,42 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import MainPageLayout from '../Components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState(' ');
+
+  const [result, setResult] = useState(null);
+
   const onInputChange = ev => {
     setInput(ev.target.value);
   };
 
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResult(result);
+    });
   };
 
   const onKeyDown = ev => {
     if (ev.keyCode === 13) onSearch();
+  };
+
+  const renderResults = () => {
+    if (result && result.length === 0) {
+      return <div>No Results</div>;
+    }
+
+    if (result && result.length > 0) {
+      return (
+        <div>
+          {result.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -30,6 +50,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
