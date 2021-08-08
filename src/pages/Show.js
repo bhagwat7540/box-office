d37c41ab55dd25b2e5a947da27327d2bff158fff
bhagwat7540 <bhagwatmiglani@gmail.com>
2021-08-08 17:39:25 +0530

    added show page

---
+++ src/pages/Show.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { apiGet } from '../misc/config';
+
+const Show = () => {
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(result => {
+      setShow(result);
+    });
+  }, [id]);
+
+  console.log('show', show);
+
+  return <div>This is show page</div>;
+};
+
+export default Show;
